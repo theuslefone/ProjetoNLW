604a99ebb86639917fc1ead7f77b9e8f53b47056
theuslefone <matheuskeeven22@gmail.com>
2021-04-20 20:55:21 -0300

    Adiciona header e player no App, para aparecer em todas as páginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '../styles/global.scss'
+
+import styles from '../styles/app.module.scss'
+
+import Player from '../components/player'
+import Header from "../components/Header";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <div className={styles.appWrapper}>
+    <main><Header />
+    <Component {...pageProps} />
+    </main>
+    <Player />
+    </div>
+    
+  );
 }
 
 export default MyApp
