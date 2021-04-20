0f63b25a89d1e62ffb95b483f494f1e55f4563c3
theuslefone <matheuskeeven22@gmail.com>
2021-04-20 16:05:13 -0300

    Adiciona Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="/logo.svg" alt="Podcastr" />
+
+            <p>O melhor para você ouvir sempre</p>
+            <span>Qui, 8 Abril</span>
+        </header>
+    )
+}
