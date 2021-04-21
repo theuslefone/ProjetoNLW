7166706658298add08f403b1556577d869908a4b
theuslefone <matheuskeeven22@gmail.com>
2021-04-21 17:00:36 -0300

    configura geração estática da page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,22 @@
 
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <div></div>
+    <div>
+      <h1>index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   );
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return{
+    props : {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
