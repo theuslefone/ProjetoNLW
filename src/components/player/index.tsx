7f7f66c000d2b364a5f45524e283a3c8a319d17f
theuslefone <matheuskeeven22@gmail.com>
2021-04-20 21:44:53 -0300

    Configura componente

--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -1,7 +1,18 @@
-import styles from './styles.module.sass'
+import styles from './styles.module.scss'
 
 export default function Player(){
     return(
-        <div></div>
+        <div className={styles.playerContainer}>
+            <header>
+                <img src="/playing.svg" alt="Tocando agora"/>
+                <strong>Tocando agora</strong>
+            </header>
+
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um podcast pra ouvir</strong>
+            </div>
+
+            <footer></footer>
+        </div>
     );
 }
