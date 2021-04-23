471758e49adff8f9e78f6e293d88efc46c6135b1
theuslefone <matheuskeeven22@gmail.com>
2021-04-23 00:20:52 -0300

    Configura página estática

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import {GetStaticProps} from 'next'
+import { api } from '../../services/api';
 
 export default function Episode(){
     const router = useRouter();
@@ -6,4 +8,14 @@ export default function Episode(){
     return(
         <h1>{router.query.slug}</h1>
     )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx)=>{
+   const { slug } = ctx.params;
+
+    const { data } = await api.get(`/episodes/${slug}`)
+    return{
+        props: {},
+        revalidate: 60*60*24, // 24h
+    }
 }
