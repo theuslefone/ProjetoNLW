149738f020d84d287bb422e8cff934eee3575039
theuslefone <matheuskeeven22@gmail.com>
2021-04-22 21:35:36 -0300

    Corrige erro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home(props, HomeProps) {
 
      </section>
 
-     <section classNmae = {styles.allEpisodes}>
+     <section className = {styles.allEpisodes}>
 
      </section>
     </div>
