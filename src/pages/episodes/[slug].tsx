7bae439f9743bb34fc92fc23c23dbb7b34b64b5d
theuslefone <matheuskeeven22@gmail.com>
2021-04-23 00:28:03 -0300

    importa pendencias, cria const episode

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,4 +1,7 @@
 import { useRouter } from 'next/router';
+import ptBR from   'date-fns/locale/pt-BR';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import {format, parseISO} from 'date-fns'
 import {GetStaticProps} from 'next'
 import { api } from '../../services/api';
 
@@ -14,6 +17,17 @@ export const getStaticProps: GetStaticProps = async (ctx)=>{
    const { slug } = ctx.params;
 
     const { data } = await api.get(`/episodes/${slug}`)
+    const episode = {
+        id: data.id,
+        title: data.title,
+        thumbnail: data.thumbnail,
+        members: data.members,
+        publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+        duration: Number(data.file.duration),
+        durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+        description: data.description,
+        url: data.file.url,
+    }
     return{
         props: {},
         revalidate: 60*60*24, // 24h
