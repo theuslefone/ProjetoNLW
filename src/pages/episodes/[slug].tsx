672f635861777f063b3a33950fdfa65b402b74cf
theuslefone <matheuskeeven22@gmail.com>
2021-04-23 01:24:15 -0300

    Corrige

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,9 +1,11 @@
 import { useRouter } from 'next/router';
-import ptBR from   'date-fns/locale/pt-BR';
+import ptBR from 'date-fns/locale/pt-BR';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
-import {format, parseISO} from 'date-fns'
-import {GetStaticPaths, GetStaticProps} from 'next'
+import { format, parseISO } from 'date-fns'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { api } from '../../services/api';
+
+import styles from '.episode.module.scss'
 
 type Episode = {
     id: string,
@@ -16,31 +18,21 @@ type Episode = {
     description: string,
     url: string
 };
-
 type EpisodeProps = {
     episode: Episode;
-};
-
-export const getStaticPaths: GetStaticPaths = async () =>{
-    return {
-        paths: [],
-        fallback: 'blocking'
-    }
 }
 
-
-export default function Episode({episode}){
-    const router = useRouter();
-
-    return(
+export default function Episode({ episode }:EpisodeProps ) {
+    return (
         <h1>{episode.title}</h1>
     )
 }
 
-export const getStaticProps: GetStaticProps = async (ctx)=>{
-   const { slug } = ctx.params;
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const { slug } = ctx.params;
 
     const { data } = await api.get(`/episodes/${slug}`)
+
     const episode = {
         id: data.id,
         title: data.title,
@@ -52,8 +44,10 @@ export const getStaticProps: GetStaticProps = async (ctx)=>{
         description: data.description,
         url: data.file.url,
     }
-    return{
-        props: {},
-        revalidate: 60*60*24, // 24h
+    return {
+        props: {
+            episode,
+        },
+        revalidate: 60 * 60 * 24, // 24h
     }
 }
