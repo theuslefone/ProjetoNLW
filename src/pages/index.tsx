16c38c98754d07cab77eba490df4f6e9fb1db923
theuslefone <matheuskeeven22@gmail.com>
2021-04-22 21:19:04 -0300

    adiciona função

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import {format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import {convertDurationToTimeString} from '../utils/convertDurationToTimeString';
 import { api } from '../services/api';
 
 type episode = {
@@ -42,16 +43,17 @@ export const getStaticProps: GetStaticProps = async () => {
       members: episode.members,
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
       duration : Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
       description : episode.description,
       url: episode.file.url,
-      
+
     }
   })
 
 
   return {
     props: {
-      episodes: data,
+      episodes,
 
     },
     revalidate: 60 * 60 * 8
