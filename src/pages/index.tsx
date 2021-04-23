720b1c1c024428b8093b2ebb92c29672dac10d25
theuslefone <matheuskeeven22@gmail.com>
2021-04-22 21:04:09 -0300

    exporta data-fns, inicia formatação de dados recebidos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import {format, parseISO} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 
 type episode = {
@@ -29,6 +31,22 @@ export const getStaticProps: GetStaticProps = async () => {
       order: 'desc',
     }
   });
+
+  // Formatar dados da API
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration : Number(episode.file.duration),
+      description : episode.description,
+      url: episode.file.url,
+      
+    }
+  })
 
 
   return {
