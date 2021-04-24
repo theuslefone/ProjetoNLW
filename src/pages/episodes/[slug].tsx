0491f97a2a5cd03684539766f6dbd24175c2b980
theuslefone <matheuskeeven22@gmail.com>
2021-04-24 15:34:46 -0300

    Configura user rout, configura geração estática com limt 2

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -25,6 +25,16 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }:EpisodeProps ) {
+
+  const router = useRouter();
+  if(router.isFallback){
+    return(
+      <div>
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
     return (
         <div className={styles.episode}>
         <div className={styles.thumbnailContainer}>
@@ -57,8 +67,23 @@ export default function Episode({ episode }:EpisodeProps ) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      order: 'desc',
+    }
+  });
+
+  const paths = data.map(episode =>{
     return{
-        paths: [],
+      params:{
+        slug : episode.id
+      }
+    }
+  })
+    return{
+        paths,
         fallback: 'blocking'
     }
 }
