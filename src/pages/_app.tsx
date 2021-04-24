6b76adc9daddf6bb336e46b5152a0aef5f590d99
theuslefone <matheuskeeven22@gmail.com>
2021-04-24 16:16:14 -0300

    importo context e configura function play

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,31 @@
+import { useState } from 'react';
+
 import '../styles/global.scss'
-
 import styles from '../styles/app.module.scss'
-
 import Player from '../components/player'
 import Header from "../components/Header";
+import { playerContext } from '../context/playerContext';
+
 
 function MyApp({ Component, pageProps }) {
 
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+  function play(episode){
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.appWrapper}>
-    <main><Header />
-    <Component {...pageProps} />
-    </main>
-    <Player />
-    </div>
+    <playerContext.Provider value={{episodeList, currentEpisodeIndex, play}}>
+      <div className={styles.appWrapper}>
+      <main><Header />
+      <Component {...pageProps} />
+      </main>
+      <Player />
+      </div>
+    </playerContext.Provider>
     
   );
 }
